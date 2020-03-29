00792a5d6bfab6d9d200464d7ab5d0b96fd85a44
nicomontale <roxas.nm@gmail.com>
2020-03-29 18:47:41 +0200

    progress session

--- src/components/WorkoutList/WorkoutItem.js
+++ src/components/WorkoutList/WorkoutItem.js
@@ -2,26 +2,30 @@ import React from 'react'
 import '../../styles/workoutitem.css';
 
 export default function WorkoutItem({
-    name, reps, id, date, onDelete
+    name, reps, id, date, onDelete, progress
 }
 
 ) {
     return (
-        <li onClick={
-            () => onDelete(id)
-        }
-
-        > <div className="workout-item"> <span className="workout_item_name"> {
-            name
-        }
-
-        </span> <span className="workout_item_count"> {
-            reps
-        }
-
-                    ripetizioni</span> <span className="workout_item_date"> {
-                        date
-                    }
+        <li onClick=
+            {
+                () => onDelete(id)
+            }>
+            <div className="workout-item">
+                <span className="workout_item_name">
+                    {name}
+                </span>
+                <span
+                    className={`workout_item_count ${
+                        progress ? (progress > 0 && "up") || "down" : ""
+                        }`}
+                >
+                    {reps} reps
+        {progress ? (progress > 0 && ` ${progress}%↑`) || ` ${progress}%↓` : ""}
+                </span>
+                <span className="workout_item_date"> {
+                    date
+                }
 
                 </span> <span className="cross">X</span> </div> </li>)
 }
