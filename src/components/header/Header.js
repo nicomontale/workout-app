660d1c691f2a7e2240e3aa63ecb8d484e4d5a098
nicomontale <roxas.nm@gmail.com>
2020-03-30 13:10:19 +0200

    title

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header className="header">
 
-            <h1>Workout Diary</h1>
+            <h1>Workout Progress</h1>
         </header>
     )
 }
